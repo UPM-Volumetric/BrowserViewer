40a2f30b0a4ee35d454f5ec6b0fe86210992adf3
Olivier Brochu <olivier_brochu@hotmail.com>
2024-06-16 13:01:16 +0200

    Add LOD download

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ Software that can view point clouds in your browser using Three.js.
 ## Installation
 
 1. Install [Node.js 22](https://nodejs.org/en/download/package-manager)
-2. Install the dependencies : `npm install`
+1. Install the dependencies : `npm install`
 
 ## Run
 
-Run `npm run start` at the root of this directory and open the URL in a web browser.
+1. Use the tiling software to create a tiled point cloud.
+1. Change the manifest path in the `src/tiled.js` file.
+1. Run `npm run start` at the root of this directory and open the URL in a web browser.
--- src/tiled.js
+++ src/tiled.js
@@ -11,7 +11,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(1, 1, 1);
 
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.01, 10000000000);
-camera.position.set(0, 0, 5);
+camera.position.set(10000, 0, 10000);
 
 const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
 
@@ -29,33 +29,79 @@ for (var i = 0; i < manifest.tiles.length; i++)
 
     // Make an invisible cube in the viewport for each tile
     const geometry = new THREE.BoxGeometry(tile.width, tile.height, tile.depth);
-    const material = new THREE.MeshBasicMaterial({color: 0x00ff00, transparent: true, opacity: 0});
+    const material = new THREE.MeshBasicMaterial({color: 0x00ff00, transparent: true, opacity: 0.2});
     const cube = new THREE.Mesh(geometry, material);
 
     cube.position.set(tile.x, tile.y, tile.z);
-
+    cube.userData.tile = tile;
+    cube.userData.tile.lod = Infinity;
+    
     scene.add(cube);
 
-    geometry.userData.tile = tile;
-
-    // Load the tile if the invisible cube is in the viewport
+    // Load the proper LOD if the tile is in the viewport
     cube.onBeforeRender = function (renderer, scene, camera, geometry, material, group)
     {
-        if (!geometry.userData.loaded)
+        // Get the distance between this object and the camera
+        var distanceWithCamera = camera.position.distanceTo(this.position);
+
+        // Determine the LOD to load
+        var lodIndex = chooseLod(distanceWithCamera, 1000, this.userData.tile.representations);
+        var lod = this.userData.tile.representations[lodIndex].segment
+
+        // Load the LOD if it has not been done yet or if we need a better LOD
+        if (lodIndex < this.userData.tile.lod)
         {
-            // Load the tile
+            this.userData.tile.lod = lodIndex;
+            
             const loader = new PLYLoader();
 
-            loader.load(geometry.userData.tile.segment, (data) => {
+            loader.load(lod, (data) =>
+            {
                 const material = new THREE.PointsMaterial({size: 0.1, vertexColors: true});
                 const points = new THREE.Points(data, material);
-                
-                scene.add(points);
-            });
 
-            geometry.userData.loaded = true;
+                points.name = lod;
+
+                scene.add(points);
+
+                // Remove the old LOD
+                if (this.userData.tile.pointsName != undefined)
+                {
+                    var selectedObject = scene.getObjectByName(this.userData.tile.pointsName);
+                    scene.remove(selectedObject);
+                }
+
+                this.userData.tile.lodIndex = lodIndex;
+                this.userData.tile.pointsName = points.name;
+            }, null, (error) => {
+                console.log(error);
+            });
         }
     }
+}
+
+function chooseLod(distance, minDistance, lods)
+{
+    // Reverse sort the LODs by number of points
+    lods.sort((a, b) =>
+    {
+        return b.points- a.points;
+    });
+
+    if (lods.length == 1 || distance <= minDistance)
+        return 0;
+
+    for (var i = 1; i < lods.length; i++)
+    {
+        // Get the ratio between the current LOD and the best LOD
+        var ratio = lods[0].points / lods[i].points;
+
+        if (distance <= ratio * minDistance)
+            return i;
+    }
+
+    // Otherwise, return the worst LOD
+    return lods.length - 1;
 }
 
 // Add the grid
