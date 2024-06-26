77ec707fc7183087be4f30ee9c85be0174bec5a9
Olivier Brochu <olivier_brochu@hotmail.com>
2024-06-26 11:18:18 +0200

    Add stats

--- src/tiled.js
+++ src/tiled.js
@@ -1,10 +1,39 @@
 import * as THREE from "three";
 import CameraControls from "camera-controls";
-import {PLYLoader} from "three/examples/jsm/loaders/PLYLoader.js";
+import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader.js";
+import Stats from "three/addons/libs/stats.module.js";
+
+const pointCloudeVertexShader = `
+    precision highp float;
+    varying vec3 vColor;
+
+    void main() {
+        vColor = color;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+        float dx = pow(position[0] - cameraPosition[0], 2.0);
+        float dy = pow(position[1] - cameraPosition[1], 2.0);
+        float dz = pow(position[2] - cameraPosition[2], 2.0);
+        float delta  = pow(dx + dy + dz, 0.5);
+
+        gl_PointSize = 1300.0 / delta; // The factor is model dependant
+    }
+`;
+
+const pointCloudFragmentShader = `
+    // TODO The color is too dark
+    precision highp float;
+    varying vec3 vColor;
+
+    void main() {
+        gl_FragColor = vec4(vColor, 1);
+    }
+`; // or THREE.ShaderLib["points"].fragmentShader
 
 class Tile
 {
-    static #material = new THREE.MeshBasicMaterial({color: 0x00ff00, transparent: true, opacity: 0.0});
+    static #tileMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00, transparent: true, opacity: 0.0});
+    static #pointCloudMaterial = new THREE.ShaderMaterial({vertexColors: true, vertexShader: pointCloudeVertexShader, fragmentShader: pointCloudFragmentShader});
     #tile;
     #scene;
     #cube;
@@ -24,7 +53,7 @@ class Tile
     {
         var geometry = new THREE.BoxGeometry(this.#tile.width, this.#tile.height, this.#tile.depth);
         
-        this.#cube = new THREE.Mesh(geometry, Tile.#material);
+        this.#cube = new THREE.Mesh(geometry, Tile.#tileMaterial);
         this.#cube.position.set(this.#tile.x, this.#tile.y, this.#tile.z);
         this.#cube.userData.tile = this;
 
@@ -48,37 +77,7 @@ class Tile
             loader.load(uri, (data) =>
             {
                 // const material = new THREE.PointsMaterial({size: 0.1, sizeAttenuation: false, vertexColors: true});
-
-                const material = new THREE.ShaderMaterial({
-                    vertexColors: true,
-                    vertexShader: `
-                        precision highp float;
-                        varying vec3 vColor;
-        
-                        void main() {
-                            vColor = color;
-                            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-
-                            float dx = pow(position[0] - cameraPosition[0], 2.0);
-                            float dy = pow(position[1] - cameraPosition[1], 2.0);
-                            float dz = pow(position[2] - cameraPosition[2], 2.0);
-                            float delta  = pow(dx + dy + dz, 0.5);
-
-                            gl_PointSize = 1300.0 / delta; // The factor is model dependant
-                        }
-                    `,
-                    fragmentShader: /*THREE.ShaderLib["points"].fragmentShader*/`
-                        // TODO The color is too dark
-                        precision highp float;
-                        varying vec3 vColor;
-
-                        void main() {
-                            gl_FragColor = vec4(vColor, 1);
-                        }
-                    `
-                });
-
-                const points = new THREE.Points(data, material);
+                const points = new THREE.Points(data, Tile.#pointCloudMaterial);
     
                 this.#points = points;
                 this.#scene.add(points);
@@ -99,7 +98,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(1, 1, 1);
 
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.01, 10000000000);
-camera.position.set(600, 100, 600);
+camera.position.set(600, 800, 600);
 
 const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
 
@@ -124,6 +123,12 @@ for (var i = 0; i < manifest.tiles.length; i++)
 const gridHelper = new THREE.GridHelper(50, 50);
 scene.add(gridHelper);
 
+// Stats
+var stats = new Stats();
+
+stats.showPanel(0);
+document.body.appendChild(stats.dom);
+
 // Animate
 function animate ()
 {
@@ -133,6 +138,8 @@ function animate ()
 	requestAnimationFrame(animate);
 
     renderer.render(scene, camera);
+
+    stats.update();
 }
 
 animate();
